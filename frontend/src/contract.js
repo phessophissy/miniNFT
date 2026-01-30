da8cc9176348df51e7486194f118554db9af2c60
MiniNFT Developer <developer@mininft.io>
2026-01-30 19:49:52 +0100

    config(frontend): update MAX_SUPPLY to 1005

--- frontend/src/contract.js
+++ frontend/src/contract.js
@@ -84,4 +84,4 @@ export const CONTRACT_ADDRESS = '0x80203c0838a1cABe0eAbc0aC9e22f6Abd512cAa9';
 export const MINT_PRICE = '0.000001';
 
 // Max supply
-export const MAX_SUPPLY = 505;
+export const MAX_SUPPLY = 1005;
